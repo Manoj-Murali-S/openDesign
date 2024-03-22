886426c765e6bea078621dba843ec0b5cbb8da5e
Murali <113785117+Manoj-Murali-s@users.noreply.github.com>
2024-03-22 05:38:49 +0000

    restart

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,9 +1,66 @@
 const Footer = () => {
-    return (
-      <div>
-        <h1>Hello, Footer</h1>
-      </div>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <div>
+      <footer class="footer">
+        <div className="container">
+          <div className="footerrel">
+            <img src="/assets/Frame28.png" alt="" srcset="" />
+            <div className="footerabs">
+            <div className="enq gradient">ENQUIRY NOW
+            <hr className="thin-line2"/>
+            </div>
+            <h4 className="socio">SOCIAL MEDIA</h4>
+            <div class="social-links flex gap-2">
+              <a href="#"><img src="/assets/image 8.png" alt="" srcset="" /></a>
+              <a href="#"><img src="/assets/image 9.png" alt="" srcset="" /></a>
+              <a href="#"><img src="/assets/image 10.png" alt="" srcset="" /></a>
+              <a href="#"><img src="/assets/image 11.png" alt="" srcset="" /></a>
+            </div></div>
+          </div> </div>
+        <div class="containerf row">
+          <div class="footer-col">
+
+            <ul>
+              <li><a href="#">about us</a></li>
+              <li><a href="#">our services</a></li>
+              <li><a href="#">privacy policy</a></li>
+              <li><a href="#">visit website</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">shipping</a></li>
+              <li><a href="#">returns</a></li>
+              <li><a href="#">order status</a></li>
+              <li><a href="#">payment options</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">shipping</a></li>
+              <li><a href="#">returns</a></li>
+              <li><a href="#">order status</a></li>
+              <li><a href="#">payment options</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+
+            <ul>
+              <li><a href="#">download</a></li>
+              <li><a href="#">changelog</a></li>
+              <li><a href="#">github</a></li>
+              <li><a href="#">all version</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
